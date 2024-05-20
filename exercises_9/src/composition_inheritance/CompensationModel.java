da9cc90dfbdf5523482e2869c41a7bb2277ae269
Alain Mbunkur <130954342+Mbunkur@users.noreply.github.com>
2024-05-20 22:25:07 +0100

    Update CompensationModel.java

--- exercises_9/src/composition_inheritance/CompensationModel.java
+++ exercises_9/src/composition_inheritance/CompensationModel.java
@@ -1,6 +1,8 @@
 package composition_inheritance;
 // base class for CompensationModel
 public abstract class CompensationModel {
+
+    //superclass earnings method
     public double earnings(){
         return 0;
 }
